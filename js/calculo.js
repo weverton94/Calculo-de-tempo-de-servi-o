207d1b2a9bfa4fc7eb2c37cd46446ca4358f0ff5
Weverton Sidnésio <60427996+weverton94@users.noreply.github.com>
2020-08-30 18:01:25 -0300

    Add files via upload

---
+++ js/calculo.js
@@ -0,0 +1,20 @@
+﻿
+    function formula(){
+        var numero = parseInt(document.getElementById("entrada").value);
+        var ano = parseInt(numero / 365);
+        var ano1 = ano * 365;
+        var result1 = numero - ano1;
+        var mes = parseInt(result1 / 30);
+        var mes1 = mes * 30;
+        var dia = result1 - mes1;
+
+        document.getElementById("anos").innerHTML = ano;
+        document.getElementById("meses").innerHTML = mes;
+        document.getElementById("dias").innerHTML = dia;
+                
+    }
+
+    
+    
+
+
